8180be97b58372591030eae78dd6ed4086befd87
thdangtr <dang@192.168.1.4.non-exists.ptr.local>
2024-12-27 23:05:58 +0700

    update price update logic

--- src/app/process.py
+++ src/app/process.py
@@ -13,54 +13,82 @@ from app.decorators import retry_on_fail
 def find_offer_min_price(
     crwl_offers: list[Offer],
     blacklist: list[str],
+    product_min_price: float,
+    product_max_price: float | None,
 ) -> tuple[Offer | None, Offer | None]:
     my_seller_offer = None
     other_offer_min_price = None
     for crwl_offer in crwl_offers:
+        # If current crawled offer is our
         if crwl_offer.seller == os.environ["MY_SELLER_NAME"]:
             my_seller_offer = crwl_offer
 
+        # If current crawled offer is not our and not in blacklist
         elif crwl_offer.seller not in blacklist:
-            if other_offer_min_price is None:
-                other_offer_min_price = crwl_offer
-            elif crwl_offer.price < other_offer_min_price.price:
-                other_offer_min_price = crwl_offer
+            # If it has product max price --->  product min price <= current crawled offer's price <= product max price
+            # If it hasn't product max price ---> product min price <= current crawled offer's price
+            if (
+                product_max_price is not None
+                and product_min_price <= crwl_offer.price <= product_max_price
+            ) or (product_max_price is None and product_min_price <= crwl_offer.price):
+                if other_offer_min_price is None:
+                    other_offer_min_price = crwl_offer
+                elif crwl_offer.price < other_offer_min_price.price:
+                    other_offer_min_price = crwl_offer
 
     return my_seller_offer, other_offer_min_price
 
 
 def calculate_price_change(
     product: Product,
-    other_offer_min_price: Offer,
+    other_offer_min_price: Offer | None,
+    product_min_price: float,
+    product_max_price: float | None,
 ) -> CurrencyProcessResult:
-    compare_price = other_offer_min_price.price
-
-    min_price = product.min_price()
-    max_price = product.max_price()
-
     final_price = None
 
-    if compare_price <= min_price:
-        final_price = min_price
+    # If it has no other valid offer
+    if other_offer_min_price is None:
+        # Set price to max price if it has product max price
+        if product_max_price is not None:
+            final_price = product_max_price
+        # If it has no product max price, set by product min price
+        else:
+            final_price = product_min_price
 
-    elif max_price is not None and compare_price >= max_price:
-        final_price = max_price
-
-    else:
-        final_price = round(
-            compare_price
-            - random.uniform(product.DONGIAGIAM_MIN, product.DONGIAGIAM_MAX),
-            product.DONGIA_LAMTRON,
+        return CurrencyProcessResult(
+            final_price=final_price,
+            stock=product.stock(),
+            min_price=product_min_price,
+            max_price=product_max_price,
+            compare_price=0,
+            seller="",
         )
 
-    return CurrencyProcessResult(
-        final_price=final_price,
-        stock=product.stock(),
-        min_price=min_price,
-        max_price=max_price,
-        compare_price=compare_price,
-        seller=other_offer_min_price.seller,
-    )
+    else:
+        compare_price = other_offer_min_price.price
+
+        if compare_price <= product_min_price:
+            final_price = product_min_price
+
+        elif product_max_price is not None and compare_price >= product_max_price:
+            final_price = product_max_price
+
+        else:
+            final_price = round(
+                compare_price
+                - random.uniform(product.DONGIAGIAM_MIN, product.DONGIAGIAM_MAX),
+                product.DONGIA_LAMTRON,
+            )
+
+        return CurrencyProcessResult(
+            final_price=final_price,
+            stock=product.stock(),
+            min_price=product_min_price,
+            max_price=product_max_price,
+            compare_price=compare_price,
+            seller=other_offer_min_price.seller,
+        )
 
 
 @retry_on_fail()
@@ -116,27 +144,32 @@ def currency_process(
     crwl_offers = currencies_extract(sb, url_product_compare)
     blacklist = product.blacklits()
 
+    product_min_price = product.min_price()
+    product_max_price = product.max_price()
+
     my_seller_offer, other_offer_min_price = find_offer_min_price(
-        crwl_offers, blacklist
+        crwl_offers, blacklist, product_min_price, product_max_price
     )
     now = datetime.now()
+
+    # If have no valid other crawled, update product price to product max price if existing or product min price if no
     if other_offer_min_price is None:
-        if my_seller_offer is None:
-            note_message_var = f"{last_update_message(now)}: No need to change price because nothing to compare"
-            logger.info(note_message_var)
-            product.Last_update = last_update_message(now)
-            product.Note = note_message_var
-            logger.info("Sheet updating")
-            product.update()
-            return
-        else:
-            note_message_var = f"{last_update_message(now)}: No need to change price because nothing to compare. Current price = {my_seller_offer.price}"
-            logger.info(note_message_var)
-            product.Last_update = last_update_message(now)
-            product.Note = note_message_var
-            logger.info("Sheet updating")
-            product.update()
-            return
+        logger.info(f"Calculating price change for {product.Product_name}")
+        currency_price_change_result = calculate_price_change(
+            product, other_offer_min_price, product_min_price, product_max_price
+        )
+        logger.info(f"Price updating for {product.Product_name}")
+        currency_price_update(product, currency_price_change_result)
+
+        note_message_var = f"""{last_update_message(now)}: Không có sản phẩm hợp lệ so sánh, Giá đã cập nhật thành công; Price = {currency_price_change_result.final_price}; Stock = {currency_price_change_result.stock}; Pricemin = {currency_price_change_result.min_price}, Pricemax = {currency_price_change_result.max_price}"""
+
+        logger.info("Sheet updating")
+        product.Last_update = last_update_message(now)
+        product.Note = note_message_var
+        product.update()
+        return currency_price_change_result
+
+    # If it contains valid other crawled offer, perform price compare and update
     else:
         if (
             my_seller_offer is not None
@@ -144,7 +177,7 @@ def currency_process(
             and my_seller_offer.price
             >= other_offer_min_price.price - product.DONGIAGIAM_MAX
         ):
-            note_message_var = f"{last_update_message(now)}: No need to change price because {os.environ["MY_SELLER_NAME"]} has smallest price already: Current price = {my_seller_offer.price}"
+            note_message_var = f"{last_update_message(now)}: Không cần cập nhật giá vì {os.environ["MY_SELLER_NAME"]} Đã có giá nhỏ nhất: Price = {my_seller_offer.price}; Stock = {product.stock()}; Pricemin = {product_min_price}, Pricemax = {product_max_price}, GiaSosanh = {other_offer_min_price.price} - Seller: {other_offer_min_price.seller}"
             logger.info(note_message_var)
             product.Last_update = last_update_message(now)
             product.Note = note_message_var
@@ -153,7 +186,7 @@ def currency_process(
         else:
             logger.info(f"Caculating price change for {product.Product_name}")
             currency_price_change_result = calculate_price_change(
-                product, other_offer_min_price
+                product, other_offer_min_price, product_min_price, product_max_price
             )
 
             logger.info(f"Price updating for {product.Product_name}")
